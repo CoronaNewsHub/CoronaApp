696a44b206c59aa4ab6ebe1855d620295461a7fb
rajinder4489 <rajinder4489@gmail.com>
2020-04-01 12:14:23 +0200

    Updates

--- rss_feed_extraction.R
+++ rss_feed_extraction.R
@@ -7,16 +7,30 @@ get.rss_format <- function(input){
   return(output)
 }
 
-convertToXML <- function(df,name){
-  xml <- xmlTree(name)
-  for (i in 1:nrow(df)) {
-    xml$addNode("item", close=FALSE)
-    for (j in names(df)) {
-      xml$addNode(j, df[i, j])
-    }
-    xml$closeTag()
+save.html_files <- function(data, data_name) {
+  doc_body <- NULL
+  for(i in 1:nrow(data)) {
+    doc_temp <- tags$html(
+      h1(data$item_title[i]),
+      p(data$item_description[i]),
+      a(href=data$item_link[i], target="_blank", "More information")
+    )
+    doc_temp$name <- NULL
+    doc_body <-c(doc_body,doc_temp)
   }
-  return(xml)
+  doc_body$name <- NULL
+  doc<-tags$html(tags$head(tags$title(data_name)),
+                 tags$body(doc_body))
+  save_html(doc, paste0(data_name, ".html"))
+}
+
+get.gover_new <- function(item_link) {
+  data <-data.frame(item_link)
+  item_title<- sapply(data$item_link, 
+                      function(url){url %>% as.character() %>% read_html() %>% 
+                          html_nodes('h1') %>% html_text()})
+  gover_new <- data.frame(cbind(item_title, item_link))
+  return(gover_new)
 }
 
 get.path <- function(list_categories) {
@@ -31,10 +45,11 @@ if(!require("tidyRSS")){
   library("tidyRSS")
 }
 
-if(!require("XML")){
-  install.packages("XML")
-  library("XML")
+if(!require("htmltools")){
+  install.packages("htmltools")
+  library("htmltools")
 }
+
 
 if(!require("rvest")){
   install.packages("rvest")
@@ -43,36 +58,28 @@ if(!require("rvest")){
 
 
 ####=================================== Code:
-#setwd(get.path("xml_files"))
+setwd(get.path("html_files"))
 # rivm news
 rivm_rss <- get.rss_format(tidyfeed("https://www.rivm.nl/nieuws/rss.xml", list = TRUE))
 rivm_rss_english <-rivm_rss$entries[grep("node", rivm_rss$entries$item_link),] # English news
-rivm_xml <- convertToXML(rivm_rss_english, "rivm_item") 
-saveXML(rivm_xml$value(), "rivm.xml")
+save.html_files(rivm_rss_english, "rivm")
+
 
 # nos new
-nos_rss_general <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsalgemeen", list = TRUE)) # general
-nos_general_xml <- convertToXML(nos_rss_general$entries, "nos_rss_general") 
-saveXML(nos_general_xml$value(), "nos_general.xml")
+#nos_rss_general <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsalgemeen", list = TRUE)) # general
+#save.html_files(nos_rss_general$entries, "nos_general")
 
-nos_rss_domestic <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsbinnenland", list = TRUE)) # domestic
-nos_domestic_xml <- convertToXML(nos_rss_domestic$entries, "nos_rss_domestic") 
-saveXML(nos_domestic_xml$value(), "nos_domestic.xml")
 
-nos_rss_abroad <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsbuitenland", list = TRUE)) # abroad
-nos_abroad_xml <- convertToXML(nos_rss_abroad$entries, "nos_rss_abroad") 
-saveXML(nos_general_xml$value(), "nos_abroad.xml")
+#nos_rss_domestic <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsbinnenland", list = TRUE)) # domestic
+#save.html_files(nos_rss_domestic$entries, "nos_domestic")
 
-                                
+#nos_rss_abroad <- get.rss_format(tidyfeed("http://feeds.nos.nl/nosnieuwsbuitenland", list = TRUE)) # abroad
+#save.html_files(nos_rss_abroad$entries, "nos_abroad")
+
+
 # goverment.nl new - no rss feed - we extract the links from latest news and create the data frame
 total_links <- html_attr(html_nodes(read_html("https://www.government.nl/latest"), "a"), "href")
 item_link <- paste0("https://www.government.nl", total_links[grep("/latest/news/2020/", total_links)])
-data <-data.frame(item_link)
-item_title<- sapply(data$item_link, 
-       function(url){url %>% as.character() %>% read_html() %>% 
-           html_nodes('h1') %>% html_text()})
-gover_new <- data.frame(cbind(item_title, item_link))
-
-gover_new_xml <- convertToXML(gover_new, "gover_new") 
-saveXML(gover_new_xml$value(), "gover_new.xml")
+gover_new <- get.gover_new(item_link)
+save.html_files(gover_new, "government")
 
