9ed3fa4473d63731aac3b77cb073caed8ddf6c8b
CoronaNewsHub <dutchcoronahackers@gmail.com>
2020-04-20 17:43:18 +0200

    Updates

--- graphs.r
+++ graphs.r
@@ -1,24 +1,24 @@
-library(ggplot2)
-library(gridExtra)
-library(reshape2)
-library(RColorBrewer)
+#library(ggplot2)
+#library(gridExtra)
+#library(reshape2)
+#library(RColorBrewer)
 
 
-f1 <- read.table("www/cases.txt", sep="\t", stringsAsFactors=F, header=T)
-f2 <- read.table("www/pro_mun.txt", sep="\t", stringsAsFactors=F, header=T)
+#f1 <- read.table("www/cases.txt", sep="\t", stringsAsFactors=F, header=T)
+#f2 <- read.table("www/pro_mun.txt", sep="\t", stringsAsFactors=F, header=T)
 
-f <- merge(f2, f1, by="Municipality", all.x=TRUE)
+#f <- merge(f2, f1, by="Municipality", all.x=TRUE)
 
-ff <- aggregate(f$Cases, by=list(f$Province), FUN=sum, na.rm=TRUE)
-colnames(ff) <- c("Provinces", "Cases")
-write.table(ff,"www/MunicipalityCases.csv",sep=",",row.names=FALSE)
+#ff <- aggregate(f$Cases, by=list(f$Province), FUN=sum, na.rm=TRUE)
+#colnames(ff) <- c("Provinces", "Cases")
+#write.table(ff,"www/MunicipalityCases.csv",sep=",",row.names=FALSE)
 
 # Also add this part to create a csv file for the numbers
-numbers=read.delim("www/numbers.txt",sep="\t",stringsAsFactors=FALSE,header=TRUE)
-write.table(numbers,"www/numbers.csv",sep=',',row.names=FALSE)
+#numbers=read.delim("www/numbers.txt",sep="\t",stringsAsFactors=FALSE,header=TRUE)
+#write.table(numbers,"www/numbers.csv",sep=',',row.names=FALSE)
 
 # read the cases file
-cases=read.delim("www/cases.csv",header=TRUE,stringsAsFactors=FALSE)
+cases=read.delim("www/cases.csv",header=TRUE,stringsAsFactors=FALSE, sep=",")
 #casesTowrite=cases[,c("Municipality","Cases","Hospitalization","Deaths","Cases per 100k","Hospitalization per 100k","Deaths per 100k")]
 casesTowrite=cases[,-which(names(cases) %in% "Population")]
 write.table(casesTowrite,"www/new_cases.csv",sep=",",row.names=FALSE)
